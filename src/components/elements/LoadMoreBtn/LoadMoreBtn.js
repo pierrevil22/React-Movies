217434f739b372fd791f2c3c419576d940660b46
Carden Pierre-Vil <pierrevil22@yahoo.com>
2020-03-01 05:06:04 -0500

    Fetching the API data and finishing the components for the movie page

--- src/components/elements/LoadMoreBtn/LoadMoreBtn.js
+++ src/components/elements/LoadMoreBtn/LoadMoreBtn.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './LoadMoreBtn.css';
 
-const LoadMoreBtn = (props) => {
-  return (
-    <div>
-      Load More
-    </div>
-  )
+const LoadMoreBtn = ({ text, onClick }) => (
+  <div className="rmdb-loadmorebtn" onClick={onClick}>
+    <p>{text}</p>
+  </div>
+)
+
+LoadMoreBtn.propTypes = {
+  text: PropTypes.string,
+  onClick: PropTypes.func
 }
 
 export default LoadMoreBtn;
